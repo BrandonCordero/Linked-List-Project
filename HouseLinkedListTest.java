cdbe635199bc86f4a76f271678160ab32ba4d8d4
https://github.com/new <bmcordero@abany.edu>
2016-10-16 19:34:29 -0400

    Creates a Node Class, Node constructor with Get and Set statements

--- HouseLinkedListTest.java
+++ HouseLinkedListTest.java
@@ -81,4 +81,36 @@ class HouseLinkedList {
     }
     
     
-    
+     private class Node {
+        Node next;
+        Object data;
+
+        // Node constructor
+        public Node(Object dataValue) {
+            next = null;
+            data = dataValue;
+        }
+
+        // another Node constructor if we want to
+        // specify the node to point to.
+        public Node(Object dataValue, Node nextValue) {
+            next = nextValue;
+            data = dataValue;
+        }
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object dataValue) {
+            data = dataValue;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node nextValue) {
+            next = nextValue;
+        }
+    }
+}
---
+++ HouseLinkedListTest.java~
@@ -0,0 +1,23 @@
+public class HouseLinkedListTest
+{
+public static void main(String[] args) {
+          HouseLinkedList lList = new HouseLinkedList();
+
+
+
+        // add elements to LinkedList
+        lList.add("Brandon");
+        lList.add("Irene");
+        lList.add("Martin");
+        lList.add("Emily");
+        lList.add("Alec");
+
+        System.out.println("lList - print houselinkedlist: " + lList);
+        System.out.println("lList.remove(2) - remove 2nd element: " + lList.remove(2));
+        System.out.println("lList - print houselinkedlist: " + lList);
+       
+    }
+
+
+
+}
