8f8c4473e7f34c4b8b77fe3f2773e95afa7386e2
https://github.com/new <bmcordero@abany.edu>
2016-10-16 19:30:35 -0400

    Creates Linked list constructor, passes nodes through list, and removes a node from the list

--- HouseLinkedListTest.java
+++ HouseLinkedListTest.java
@@ -21,3 +21,64 @@ public static void main(String[] args) {
 
 
 }
+class HouseLinkedList {
+    // reference to the head node.
+    private Node head;
+    private int listCount;
+
+    // LinkedList constructor
+    public HouseLinkedList() {
+        head = new Node(null);
+        listCount = 0;
+    }
+    
+     public void add(Object data)
+    // appends the specified element to the end of this list.
+    {
+        Node houseTemp = new Node(data);
+        Node houseCurrent = head;
+        // starting at the head node, crawl to the end of the list
+        while (houseCurrent.getNext() != null) {
+            houseCurrent = houseCurrent.getNext();
+        }
+        // the last node's "next" reference set to our new node
+        houseCurrent.setNext(houseTemp);
+        listCount++;// increment the number of elements variable
+    }
+
+   public boolean remove(int index)
+    // removes the element at the specified position in this list.
+    {
+        
+        if (index < 1 || index > size())
+            return false;
+
+        Node houseCurrent = head;
+        for (int i = 1; i < index; i++) {
+            if (houseCurrent.getNext() == null)
+                return false;
+
+            houseCurrent = houseCurrent.getNext();
+        }
+        houseCurrent.setNext(houseCurrent.getNext().getNext());
+        listCount--; // decrement the number of elements variable
+        return true;
+    }
+     public int size()
+    // returns the number of elements in this list.
+    {
+        return listCount;
+    }
+
+    public String toString() {
+        Node houseCurrent = head.getNext();
+        String output = "";
+        while (houseCurrent != null) {
+            output += "[" + houseCurrent.getData().toString() + "]";
+            houseCurrent = houseCurrent.getNext();
+        }
+        return output;
+    }
+    
+    
+    
